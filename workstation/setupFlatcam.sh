7d07d227e7b5baef3ff5d6cf9e665b7a8794256d
AntumArk <minkevicius.linas@gmail.com>
2022-12-28 14:11:26 +0200

    Added flatcam setup sript

---
+++ workstation/setupFlatcam.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+# "-e" exists on first error.
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+DIR="Flatcam"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "Fetching changes ${DIR}..."
+  cd ${DIR}
+  git fetch
+else
+    git clone https://github.com/AntumArk/Flatcam -b old_flatcam_pyqt5
+    cd ${DIR}
+fi
+
+# install flatcam deps
+chmod +x setup_ubuntu.sh
+./setup_ubuntu.sh
+
+# Create a link.
+ln -s $PWD/flatcam /home/$USER/Desktop/flatcam
