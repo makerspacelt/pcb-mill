d49da466574b63dd15dda23f2837aaaaefab2f1c
AntumArk <minkevicius.linas@gmail.com>
2022-12-28 15:04:59 +0200

    force remove old flatcam link

---
+++ workstation/setup_ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+# "-e" exists on first error.
+
+sudo apt-get install -y --no-install-recommends libpng-dev \
+                libfreetype6 \
+				libfreetype6-dev \
+				python3-dev \
+				python3-simplejson \
+				python3-numpy \
+				python3-pyqt5 \
+				python3-scipy \
+				python3-matplotlib \
+				libgeos-dev \
+				python3-pip \
+				libspatialindex-dev \
+				python3-tk \
+				python3-shapely \
+				python3-rtree \
+				python3-svg.path
+
